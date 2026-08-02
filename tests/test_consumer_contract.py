7bd3ffaba16ddf4523fbc9c7710ffed07dd355d1
gmalbert <gmalbert+github@gmail.com>
2026-08-02 13:31:37 -0400

    fix: pin EPL consumer to core 1.3.1

--- tests/test_consumer_contract.py
+++ tests/test_consumer_contract.py
@@ -7,4 +7,4 @@ def test_consumer_is_epl_configured():
     assert LEAGUE_CONFIG.football_data_div == "E0"
     assert LEAGUE_CONFIG.espn_slug == "eng.1"
     assert LEAGUE_CONFIG.sources.api_football_league_id == 39
-    assert __version__ == "1.3.0"
+    assert __version__ == "1.3.1"
